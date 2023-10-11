7c6b585bb8b0be4b7fb6d1ad1d2af47903355bf3
janmrakava <janmrakava@gmail.com>
2023-10-11 11:08:30 +0200

    project init

---
+++ server/db/createCollections.ts
@@ -0,0 +1,49 @@
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectionString: string = process.env.ATLAS_URI || '';
+
+const connectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions;
+
+const arrayCollections = [
+  'users',
+  'addresses_users',
+  'tickets',
+  'transactions',
+  'events',
+  'addresses_events',
+  'type_of_payment',
+  'credit_card',
+  'bank_transfer',
+  'paypal_info',
+  'google_pay',
+  'apple_pay',
+];
+
+mongoose
+  .connect(connectionString, connectOptions)
+  .then(() => {
+    const createCollectionsPromises = arrayCollections.map((collectionName) => {
+      return mongoose.connection
+        .createCollection(collectionName)
+        .then(() => {
+          console.log(`Collection "${collectionName}" was created.`);
+        })
+        .catch((error) => {
+          console.error(`Error when creating a collection "${collectionName}": ${error}`);
+        });
+    });
+
+    return Promise.all(createCollectionsPromises);
+  })
+  .then(() => {
+    console.log('All collections were successfully created.');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
---
+++ server/server.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import './db/conn';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
