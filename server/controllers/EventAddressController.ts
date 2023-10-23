acd94980595a520a9074962ea9b794837bf6f2ea
janmrakava <janmrakava@gmail.com>
2023-10-23 20:03:20 +0200

    route for get address by id

--- server/controllers/EventAddressController.ts
+++ server/controllers/EventAddressController.ts
@@ -8,10 +8,22 @@ EventAddressController.get('/getAll', async (req: Request, res: Response) => {
   try {
     const addresses = await EventAddress.find();
     if (!addresses) {
-      return res.status(404).json({ error: 'Events not found' });
+      return res.status(404).json({ error: 'Addresses not found' });
     }
     res.send(addresses).status(200);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+EventAddressController.get('/getOne/:id', async (req: Request, res: Response) => {
+  try {
+    const event = await EventAddress.findById(req.params.id);
+    if (!event) {
+      return res.status(404).json({ error: 'Event not found' });
+    }
+    res.send(event).status(200);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
   }
 });
