6168115f3ba4c9546c4caec8b24bcd6f52afb88a
janmrakava <janmrakava@gmail.com>
2023-10-28 16:01:02 +0200

    add redux, eslint, prettier

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './store';
+import { changeLanguage } from '../features/languageSlice';
+import './App.css';
 
 function App() {
+  const appLanguage = useSelector((state: RootState) => state.language.appLanguage);
+
+  const dispatch = useDispatch();
+
   return (
     <>
       <h1>Čus</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/store.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { configureStore } from '@reduxjs/toolkit';
+import languageReducer from '../features/languageSlice'
+
+/**
+ * * Configure redux of the app
+ */
+export const store = configureStore({
+  reducer: {
+    language: languageReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
