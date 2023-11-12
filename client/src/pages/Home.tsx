3106dedf7570ddac7256b94356ae6add20883dbf
janmrakava <janmrakava@gmail.com>
2023-11-12 12:27:52 +0100

    add loader when data is fetched

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { CircularProgress, Grid } from '@mui/material';
 import BuyMoreBanner from '../components/Banners/BuyMoreBanner';
 import FavoriteBanner from '../components/Banners/FavoriteBanner';
 import FindEventsBanner from '../components/Banners/FindEventsBanner';
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
   const userLoggedIn = true;
 
   const [activeButton, setActiveButton] = useState<string>('music');
-  const [intervalMs, setIntervalMs] = useState(30000);
+  const [intervalMs, setIntervalMs] = useState<number>(30000);
 
   const handleChangeActive = (newState: string): void => {
     setActiveButton(newState);
@@ -32,10 +32,6 @@ const Home: React.FC = () => {
     }
   );
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
   if (error) {
     return <p>Error: {error.message}</p>;
   }
@@ -46,56 +42,59 @@ const Home: React.FC = () => {
   return (
     <>
       <Hero selectedType={activeButton} handleChange={handleChangeActive} />
-      <Grid
-        container
-        spacing={0}
-        gap={3}
-        marginTop={5}
-        marginBottom={5}
-        alignItems="center"
-        justifyContent="center"
-        sx={{ minHeight: '50vh' }}>
-        <EventBanner
-          name="Placeholder name"
-          date={date}
-          place="Placeholder name"
-          popular={true}
-          ticketsSold={15647}
-          imgSrc="/placeholderimg.png"
-          wideScreen={true}
-          userLoggedIn={userLoggedIn}
-        />
-        <EventBanner
-          name="Placeholder name"
-          date={date}
-          place="Placeholder name"
-          popular={true}
-          ticketsSold={15647}
-          imgSrc="/placeholderimg.png"
-          wideScreen={false}
-          userLoggedIn={userLoggedIn}
-        />
-        <EventBanner
-          name="Placeholder name"
-          date={date}
-          place="Placeholder name"
-          popular={true}
-          ticketsSold={15647}
-          imgSrc="/placeholderimg.png"
-          wideScreen={false}
-          userLoggedIn={userLoggedIn}
-        />
-        <EventBanner
-          name="Placeholder name"
-          date={date}
-          place="Placeholder name"
-          popular={true}
-          ticketsSold={647}
-          imgSrc="/placeholderimg.png"
-          wideScreen={true}
-          userLoggedIn={userLoggedIn}
-        />
-      </Grid>
+      {isLoading && <CircularProgress />}
+      {!isLoading && (
+        <Grid
+          container
+          spacing={0}
+          gap={3}
+          marginTop={5}
+          marginBottom={5}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ minHeight: '50vh' }}>
+          <EventBanner
+            name="Placeholder name"
+            date={date}
+            place="Placeholder name"
+            popular={true}
+            ticketsSold={15647}
+            imgSrc="/placeholderimg.png"
+            wideScreen={true}
+            userLoggedIn={userLoggedIn}
+          />
+          <EventBanner
+            name="Placeholder name"
+            date={date}
+            place="Placeholder name"
+            popular={true}
+            ticketsSold={15647}
+            imgSrc="/placeholderimg.png"
+            wideScreen={false}
+            userLoggedIn={userLoggedIn}
+          />
+          <EventBanner
+            name="Placeholder name"
+            date={date}
+            place="Placeholder name"
+            popular={true}
+            ticketsSold={15647}
+            imgSrc="/placeholderimg.png"
+            wideScreen={false}
+            userLoggedIn={userLoggedIn}
+          />
+          <EventBanner
+            name="Placeholder name"
+            date={date}
+            place="Placeholder name"
+            popular={true}
+            ticketsSold={647}
+            imgSrc="/placeholderimg.png"
+            wideScreen={true}
+            userLoggedIn={userLoggedIn}
+          />
+        </Grid>
+      )}
       <FavoriteBanner />
       <FindEventsBanner />
       <Grid
@@ -145,7 +144,6 @@ const Home: React.FC = () => {
         />
       </Grid>
       <MobileAppBanner />
-
       <BuyMoreBanner />
     </>
   );
