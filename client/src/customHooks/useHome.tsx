ab1e2730e7c36befacd09528afcdc8c309fdf287
janmrakava <janmrakava@gmail.com>
2023-11-17 18:54:37 +0100

    move queries to own hook

--- client/src/customHooks/useHome.tsx
+++ client/src/customHooks/useHome.tsx
@@ -1,14 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { useState } from 'react';
+import { useEventsByCategory, useUniqueCities } from '../api/homeQueries';
+import { type ICityObj } from '../pages/Home';
+import { EventTypes, TimeTypes } from '../utils/enum';
+import { type RootState } from '../pages/store';
+import { useSelector } from 'react-redux';
+
+import { type Event } from '../utils/interfaces';
+import EventBanner from '../components/EventBanners/MobileEventBanner';
 
 export const useHome = (): any => {
+  /**
+   * !DEBUG variables
+   */
+  const userLoggedIn = true;
+
+  /**
+   * * AppLanguage
+   */
+  const appLanguage = useSelector((state: RootState) => state.language.appLanguage);
+
   const [activeButton, setActiveButton] = useState<string>('music');
 
   const handleChangeActiveButton = (newState: string): void => {
     setActiveButton(newState);
   };
 
+  /**
+   * * QUERIES   *
+   */
+  const {
+    data: eventsByCategoryData,
+    error: eventsByCategoryError,
+    isLoading: eventsByCatagoryIsLoading
+  } = useEventsByCategory(activeButton);
+
+  const {
+    data: uniqueCitiesData,
+    error: uniqueCitiesError,
+    isLoading: uniqueCitiesIsLoading
+  } = useUniqueCities();
+
+  const citiesObj = uniqueCitiesData as unknown as ICityObj[] | null;
+
+  const eventsData = eventsByCategoryData as unknown as Event[] | null;
+
+  /**
+   * * States for FindBannerEvent
+   */
+
+  const [choosedCity, setChoosedCity] = useState<string>('Praha');
+  const [chooseEventType, setChooseEventType] = useState<string>(EventTypes.MUSIC);
+  const [choosedTime, setChoosedTime] = useState<string>(TimeTypes.WEEKEND);
+
+  const handleCityChange = (newActive: string): void => {
+    setChoosedCity(newActive);
+  };
+
+  const handleEventTypeChange = (newActive: string): void => {
+    setChooseEventType(newActive);
+  };
+  const handleTimeTypeChange = (newActive: string): void => {
+    setChoosedTime(newActive);
+  };
+  /**
+   * * Render Event Banners
+   */
+  const eventBanners = eventsData ? (
+    eventsData?.map((event: Event, index: number) => {
+      const name = event.name[appLanguage];
+      return (
+        <EventBanner
+          key={index}
+          name={name || 'Unknown name'}
+          date={event.date_of_the_event}
+          place={event.address_id ? event.address_id.name_of_place : 'Unknown place'}
+          popular={event.popular || false}
+          ticketsSold={event.ticket_types.reduce((total, type) => total + type.sold, 0) || 0}
+          imgSrc={event.image}
+          wideScreen={index % 2 === 0}
+          userLoggedIn={userLoggedIn}
+        />
+      );
+    })
+  ) : (
+    <p>No data available</p>
+  );
+
   return {
     activeButton,
-    handleChangeActiveButton
+    handleChangeActiveButton,
+    citiesObj,
+    uniqueCitiesError,
+    uniqueCitiesIsLoading,
+    eventsData,
+    eventsByCategoryError,
+    eventsByCatagoryIsLoading,
+    choosedCity,
+    handleCityChange,
+    chooseEventType,
+    handleEventTypeChange,
+    choosedTime,
+    handleTimeTypeChange,
+    eventBanners
   };
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,17 +5,9 @@ import BuyMoreBanner from '../components/Banners/BuyMoreBanner';
 import FavoriteBanner from '../components/Banners/FavoriteBanner';
 import FindEventsBanner from '../components/Banners/FindEventsBanner';
 import MobileAppBanner from '../components/Banners/MobileAppBanner';
-import EventBanner from '../components/EventBanners/MobileEventBanner';
 import SearchResultBanner from '../components/EventBanners/SearchResultBanner';
 import Hero from '../components/HeroSection/Hero';
 
-import { useState } from 'react';
-import { type RootState } from './store';
-import { useSelector } from 'react-redux';
-
-import { type Event } from '../utils/interfaces';
-import { useEventsByCategory, useUniqueCities } from '../api/homeQueries';
-import { EventTypes, TimeTypes } from '../utils/enum';
 import { useHome } from '../customHooks/useHome';
 
 export interface ICityObj {
@@ -30,65 +22,26 @@ const Home: React.FC = () => {
   const date = new Date();
   const userLoggedIn = true;
 
-  const appLanguage = useSelector((state: RootState) => state.language.appLanguage);
-
-  const { activeButton, handleChangeActiveButton } = useHome();
-
   const {
-    data: eventsByCategoryData,
-    error: eventsByCategoryError,
-    isLoading: eventsByCatagoryIsLoading
-  } = useEventsByCategory(activeButton);
-
-  const {
-    data: uniqueCitiesData,
-    error: uniqueCitiesError,
-    isLoading: uniqueCitiesIsLoading
-  } = useUniqueCities();
-
-  const citiesObj = uniqueCitiesData as unknown as ICityObj[] | null;
-
-  const eventsData = eventsByCategoryData as unknown as Event[] | null;
+    activeButton,
+    handleChangeActiveButton,
+    citiesObj,
+    uniqueCitiesError,
+    uniqueCitiesIsLoading,
+    eventsByCategoryError,
+    eventsByCatagoryIsLoading,
+    choosedCity,
+    handleCityChange,
+    chooseEventType,
+    handleEventTypeChange,
+    choosedTime,
+    handleTimeTypeChange,
+    eventBanners
+  } = useHome();
 
   if (eventsByCategoryError instanceof Error) {
     return <p>Error: {eventsByCategoryError.message}</p>;
   }
-
-  const [choosedCity, setChoosedCity] = useState<string>('Praha');
-  const [chooseEventType, setChooseEventType] = useState<string>(EventTypes.MUSIC);
-  const [choosedTime, setChoosedTime] = useState<string>(TimeTypes.WEEKEND);
-
-  const handleCityChange = (newActive: string): void => {
-    setChoosedCity(newActive);
-  };
-
-  const handleEventTypeChange = (newActive: string): void => {
-    setChooseEventType(newActive);
-  };
-  const handleTimeTypeChange = (newActive: string): void => {
-    setChoosedTime(newActive);
-  };
-
-  const eventBanners = eventsData ? (
-    eventsData?.map((event: Event, index: number) => {
-      const name = event.name[appLanguage];
-      return (
-        <EventBanner
-          key={index}
-          name={name || 'Unknown name'}
-          date={event.date_of_the_event}
-          place={event.address_id ? event.address_id.name_of_place : 'Unknown place'}
-          popular={event.popular || false}
-          ticketsSold={event.ticket_types.reduce((total, type) => total + type.sold, 0) || 0}
-          imgSrc={event.image}
-          wideScreen={index % 2 === 0}
-          userLoggedIn={userLoggedIn}
-        />
-      );
-    })
-  ) : (
-    <p>No data available</p>
-  );
 
   return (
     <>
