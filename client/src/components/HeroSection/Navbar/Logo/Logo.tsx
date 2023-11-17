1a6bf4dc6f63377546a3d497934b7c3e6dee0f4b
janmrakava <janmrakava@gmail.com>
2023-11-17 21:45:07 +0100

    add link to homepage to logo

--- client/src/components/HeroSection/Navbar/Logo/Logo.tsx
+++ client/src/components/HeroSection/Navbar/Logo/Logo.tsx
@@ -9,32 +9,38 @@ import {
   LogoTypographyBig,
   LogoTypographyMedium
 } from '../../../../styles/styles';
+import { Link } from 'react-router-dom';
 
 const Logo: React.FC = () => {
   return (
     <>
       <Grid item xs={3} md={4} lg={4} sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>
-        <Typography>
-          <LogoImg src="/logo.png" alt="TicketBlock Logo" />
-        </Typography>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <Typography>
+            <LogoImg src="/logo.png" alt="TicketBlock Logo" />
+          </Typography>
+        </Link>
       </Grid>
       <Grid
         item
         xs={4}
         md={4}
         lg={4}
-        sx={{ display: { xs: 'none', sm: 'none', md: 'block', lg: 'none' } }}
-      >
-        <LogoTypographyMedium>
-          <LogoImgTextMedium src="/logo.png" alt="TicketBlock Logo" />
-          TicketBlock
-        </LogoTypographyMedium>
+        sx={{ display: { xs: 'none', sm: 'none', md: 'block', lg: 'none' } }}>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <LogoTypographyMedium>
+            <LogoImgTextMedium src="/logo.png" alt="TicketBlock Logo" />
+            TicketBlock
+          </LogoTypographyMedium>
+        </Link>
       </Grid>
       <Grid item xs={4} md={4} lg={4} sx={{ display: { xs: 'none', md: 'none', lg: 'block' } }}>
-        <LogoTypographyBig>
-          <LogoImgText src="/logo.png" alt="TicketBlock Logo" />
-          TicketBlock
-        </LogoTypographyBig>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <LogoTypographyBig>
+            <LogoImgText src="/logo.png" alt="TicketBlock Logo" />
+            TicketBlock
+          </LogoTypographyBig>
+        </Link>
       </Grid>
     </>
   );
