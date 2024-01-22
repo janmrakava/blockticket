edaad4b176f6f6090e1adf938054bda9d93101a0
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-01-22 19:56:38 +0100

    add paymentdelivery and ordertickets json files

--- client/src/components/Support/SupportSection.tsx
+++ client/src/components/Support/SupportSection.tsx
@@ -1,11 +1,40 @@
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { type RootState } from '../../pages/store';
+import { useEffect, useState } from 'react';
 
 const SupportSection: React.FC = () => {
+  const [dataSection, setDataSection] = useState();
   const location = useLocation();
 
   const text = location.state.text;
 
   console.log('SupportSection: ', text);
+
+  const appLanguage = useSelector((state: RootState) => state.language.appLanguage);
+
+  console.log(appLanguage);
+
+  /**
+   * TODO some fix with way, maybe use React Query on this?
+   */
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response = await fetch(`/supportSections/${text}.json`);
+        const data = await response.json();
+        console.log(data);
+        setDataSection(data);
+      } catch (error) {
+        console.error('Error fetching FAQ data:', error);
+      }
+    };
+
+    void fetchData();
+  }, []);
+
+  console.log(dataSection);
 
   return (
     <div style={{ color: 'white' }}>
