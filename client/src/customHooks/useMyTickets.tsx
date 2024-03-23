3dbbd53e6455333a55cd004e1ea521b3bb37e53e
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-03-23 17:31:11 +0100

    style MyTickets

--- client/src/customHooks/useMyTickets.tsx
+++ client/src/customHooks/useMyTickets.tsx
@@ -5,9 +5,12 @@ import { useState, useEffect } from 'react';
 import { type DecodedToken } from './useHome';
 import Cookies from 'universal-cookie';
 import { useGetUserTickets } from '../api/userQueries';
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { type TicketWithId } from '../utils/interfaces';
 import NoFavoritesEvents from '../components/FavoritesPage/NoFavoriteEvents';
+import { useNavigate } from 'react-router-dom';
+import { countDate } from '../utils/dateFunctions';
+import ConfirmationNumber from '@mui/icons-material/ConfirmationNumber';
 
 export const useMyTickets = (): any => {
   const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
@@ -32,9 +35,15 @@ export const useMyTickets = (): any => {
     error: ticketsDataError
   } = useGetUserTickets(userId);
 
+  const navigate = useNavigate();
+  const handleNavigate = (ticketId: string, eventID: string): void => {
+    navigate(`/ticketInfo/${eventID}/${ticketId}`);
+  };
+
   const renderTickets =
     ticketsData && ticketsData.length > 0 ? (
       ticketsData?.map((ticket: TicketWithId, index: number) => {
+        const renderDate = countDate(ticket.date);
         return (
           <Grid
             item
@@ -42,6 +51,9 @@ export const useMyTickets = (): any => {
             md={5}
             lg={3}
             key={index}
+            onClick={() => {
+              handleNavigate(ticket._id, ticket.event);
+            }}
             sx={{
               height: '400px',
               cursor: 'pointer',
@@ -54,8 +66,15 @@ export const useMyTickets = (): any => {
               backgroundSize: 'cover',
               backgroundPosition: 'center'
             }}>
-            <Typography>{ticket.event}</Typography>
-            <Typography>{ticket.name}</Typography>
+            <Box>
+              <ConfirmationNumber />
+            </Box>
+            <Box>
+              <Typography sx={{ fontSize: '30px', fontWeight: 800 }}>{ticket.name}</Typography>
+              <Typography sx={{ fontSize: '20px' }}>{renderDate}</Typography>
+              <Typography>{ticket.city}</Typography>
+              <Typography>{ticket.place}</Typography>
+            </Box>
           </Grid>
         );
       })
